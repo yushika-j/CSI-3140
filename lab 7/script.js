f4d0a152c0bed76855089d440162add1eff5189e
Yushika Jhundoo <pjhun035@uottawa.ca>
2024-03-23 15:56:29 -0400

    modified:   lab 7/index.html 	modified:   lab 7/script.js

--- lab 7/script.js
+++ lab 7/script.js
@@ -21,10 +21,49 @@ document.addEventListener(DOMContentLoaded, function(){
 
         data.forEach(function(product){
             var row = table.insertRow();
+            row.insertCell().innerHTML = product.name;
             row.insertCell().innerHTML = product.id;
-        })
+            row.insertCell().innerHTML = product.price;
+            row.insertCell().innerHTML = '<img src = "${product.thumbnail}" alt="${product.name}" class="thumbnail" />';
+
+            // Create a button for each product to display description
+            var buttonCell = row.insertCell();
+            var button = document.createElement("button");
+            button.textContent = "Show Description";
+            button.addEventListener("click", function() {
+                displayDescription(product.id);
+            });
+            buttonCell.appendChild(button);
+        });
     }
 
+    function imageHover(){
+        var images = document.querySelectorAll('.productImage');
+
+        images.forEach(function(image){
+            image.addEventListener('mouseover', function(){
+                image.style.opacity = 0.5;
+            });
+
+            image.addEventListener('mouseout', function(){
+                image.style.opacity = 1;
+            });
+        });
+    }
+
+    function displayDescription(productID){
+       // Fetch product description based on productId
+       fetchData("descriptions.json", function(descriptionsData) {
+            var description = descriptionsData.find(function(item) {
+                return item.id === productID;
+            });
+
+            // Display the description in the designated area
+            var descriptionDiv = document.getElementById("productDescription");
+            descriptionDiv.innerHTML = "<h2>Description</h2>";
+            descriptionDiv.innerHTML += "<p>" + description.description + "</p>";
+        });
+    }
 
 
 
