fedcb93c0c446902b11c4c542e12786f4eb3ab97
Yushika Jhundoo <pjhun035@uottawa.ca>
2024-03-23 14:43:55 -0400

    modified script.js

---
+++ lab 7/script.js
@@ -0,0 +1,40 @@
+document.addEventListener(DOMContentLoaded, function(){
+
+    function fetchData(url, callback){
+        var asyncRequest = new XMLHttpRequest();
+
+        asyncRequest.onreadystatechange = function(){
+            if(asyncRequest.readyState == 4 && asyncRequest.status == 200){
+                var data = JSON.parse(asyncRequest.responseText);
+                callback(data);
+            }
+        };
+
+        asyncRequest.open('GET', url, true);
+        asyncRequest.send();
+
+    }
+
+    //populate table
+    function populateTable(data){
+        var table = document.getElementById('productTable');
+
+        data.forEach(function(product){
+            var row = table.insertRow();
+            row.insertCell().innerHTML = product.id;
+        })
+    }
+
+
+
+
+
+
+
+    // Fetch data from summary.json when the page loads
+    fetchData("summary.json", function(summaryData) {
+        // Populate the table with product information
+        populateTable(summaryData);
+
+
+});
