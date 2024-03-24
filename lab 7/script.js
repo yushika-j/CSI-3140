c862d8c1d8db492d46b3243bd719ef29285532f6
Yushika Jhundoo <pjhun035@uottawa.ca>
2024-03-23 23:21:22 -0400

    modified:   lab 7/product1_thumb.jpg

--- lab 7/script.js
+++ lab 7/script.js
@@ -15,34 +15,38 @@ document.addEventListener("DOMContentLoaded", function(){
     }
 
     //populate table
-    function populateTable(data){
+    function populateTable(data) {
         var table = document.getElementById('table-body');
-
+    
         table.innerHTML = '';
-        
+    
         data.forEach(function(product) {
             var row = table.insertRow();
-        
+    
             // Insert product title
             var titleCell = row.insertCell();
             titleCell.innerHTML = product.title;
-        
+    
             // Insert product ID
             var idCell = row.insertCell();
             idCell.innerHTML = product.id;
-        
+    
             // Insert product price
             var priceCell = row.insertCell();
             priceCell.innerHTML = "$" + product.price;
-        
+    
             // Insert product thumbnail image
             var imageCell = row.insertCell();
-            imageCell.innerHTML = '<img id="product-thumb" src="' + product.thumbnail + '" alt="' + product.title + '" class="thumbnail" />';
-            
+            var thumbnailImg = document.createElement('img');
+            thumbnailImg.src = product.thumbnail;
+            thumbnailImg.alt = product.title;
+            thumbnailImg.classList.add('thumbnail');
+            thumbnailImg.setAttribute('data-product-id', product.id); // Set data-product-id attribute
+            imageCell.appendChild(thumbnailImg);
+    
             // Insert product description
             var descriptionCell = row.insertCell();
-            
-        
+    
             // Create a button for each product to display description
             var buttonCell = row.insertCell();
             var button = document.createElement("button");
@@ -51,22 +55,43 @@ document.addEventListener("DOMContentLoaded", function(){
                 displayDescription(product.id, descriptionCell);
             });
             buttonCell.appendChild(button);
-            
         });
-        
+
+    
+        // Add hover effect to product images after populating the table
+        imageHover();
     }
-
+    
+        
     function imageHover(){
-        var images = document.querySelectorAll('.productImage');
-
+        var images = document.querySelectorAll('.thumbnail');
+        
         images.forEach(function(image){
             image.addEventListener('mouseover', function(){
-                image.style.opacity = 0.5;
+                var productID = image.getAttribute('data-product-id');
+                image.src = "product" + productID + "_thumb.jpg";
+                image.style.transform = 'scale(1.2)';
+                image.style.transition = 'transform 0.5s';
+                var productID = image.getAttribute('data-product-id');
+                fetchFullImage(productID);
             });
 
             image.addEventListener('mouseout', function(){
-                image.style.opacity = 1;
+                image.style.transform = 'scale(1)';
+                image.src = "product" + productID + ".jpg";
             });
+        });
+    }
+
+    function fetchFullImage(productID){
+        fetchData("full_images.json", function(imagesData) {
+            var fullImage = imagesData.find(function(item) {
+                return item.id === productID;
+            });
+            if(fullImage){
+                var imageCell = document.querySelector('img[data-product-id="' + productID + '"]');
+                imageCell.src = fullImage.image;
+            }
         });
     }
 
