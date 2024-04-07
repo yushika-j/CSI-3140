a003edf243939a907afb7e338157b8c3985a17bc
Yushika Jhundoo <pjhun035@uottawa.ca>
2024-04-06 22:32:58 -0400

    new file:   lab 7.zip 	new file:   practice midterm/assignment8/ex19.4.php 	new file:   practice midterm/assignment8/states.php

---
+++ practice midterm/assignment8/ex19.4.php
@@ -0,0 +1,11 @@
+<!-- Yushika Jhundoo - 300269830
+Nidhi Thakkar - 300202450 -->
+
+
+$pattern = '^A[A-Za-z0-9]{3}[Bb]+\d{2}$/';
+
+<!-- 
+A matches the uppercase letter A.
+[A-Za-z0-9]{3} matches any three alphanumeric characters.
+[Bb]+ matches one or more occurrences of the uppercase or lowercase letter B.
+\d{2} matches exactly two digits. -->
---
+++ practice midterm/assignment8/states.php
@@ -0,0 +1,32 @@
+<?php
+
+// Define the states string
+$states = "Mississippi Alabama Texas Massachusetts Kansas";
+
+// Initialize the array to store matched states
+$statesArray = array();
+
+// Task a)
+preg_match('/\bxas\b/', $states, $matches);
+$statesArray[0] = $matches[0];
+
+// Task b)
+preg_match('\b^k\w+s$\b', $states, $matches);
+$statesArray[1] = $matches[0];
+
+// Task c)
+preg_match('/\b^M\w+s$\b/', $states, $matches);
+$statesArray[2] = $matches[0];
+
+// Task d)
+preg_match('/\b\w+a$\b/', $states, $matches);
+$statesArray[3] = $matches[0];
+
+// Task e)
+preg_match('/\b^M\w+/', $states, $matches);
+$statesArray[4] = $matches[0];
+
+// Output the array
+print_r($statesArray);
+
+?>
